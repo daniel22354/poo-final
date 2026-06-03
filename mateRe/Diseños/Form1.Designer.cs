1166d0c1a09ad3743c58cacd3e78bfa0605b473b
daniel22354 <danielco252501@gmail.com>
2026-06-02 20:14:47 -0600

    Remove summary comments from Dispose method
    Removed XML summary comments from Dispose method.

--- mateRe/Diseños/Form1.Designer.cs
+++ mateRe/Diseños/Form1.Designer.cs
@@ -5,10 +5,8 @@
 		
 		private System.ComponentModel.IContainer components = null;
 
-		/// <summary>
-		///  Clean up any resources being used.
-		/// </summary>
-		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		
+		
 		protected override void Dispose(bool disposing)
 		{
 			if (disposing && (components != null))
